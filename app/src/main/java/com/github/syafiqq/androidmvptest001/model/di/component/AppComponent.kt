6ddcf7237d132adc8dee921cfdeaabf3890fa6dc
Syafiq <syafiq.rezpector@gmail.com>
2019-09-26 14:06:27 +0700

    Add app component

---
+++ app/src/main/java/com/github/syafiqq/androidmvptest001/model/di/component/AppComponent.kt
@@ -0,0 +1,18 @@
+package com.github.syafiqq.androidmvptest001.model.di.component
+
+import com.github.syafiqq.androidmvptest001.controller.App
+import com.github.syafiqq.androidmvptest001.model.di.module.AppModule
+import dagger.Component
+import dagger.android.AndroidInjectionModule
+import dagger.android.AndroidInjector
+
+@Component(
+    modules = [
+        AndroidInjectionModule::class,
+        AppModule::class
+    ]
+)
+interface AppComponent : AndroidInjector<App> {
+    @Component.Factory
+    interface Factory : AndroidInjector.Factory<App>
+}
