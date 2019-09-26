87d2d52b8aee1c8e1508efe359c52f5239cf063c
Syafiq <syafiq.rezpector@gmail.com>
2019-09-26 14:00:50 +0700

    add scope

---
+++ app/src/main/java/com/github/syafiqq/androidmvptest001/model/di/scope/ActivityScope.kt
@@ -0,0 +1,8 @@
+package com.github.syafiqq.androidmvptest001.model.di.scope
+
+import javax.inject.Scope
+
+@Scope
+@MustBeDocumented
+@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
+annotation class ActivityScope
