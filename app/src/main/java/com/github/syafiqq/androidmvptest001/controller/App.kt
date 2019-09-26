1eb95ebd4ee00bb1c2ee3a6e7693a8dea828ec22
Syafiq <syafiq.rezpector@gmail.com>
2019-09-26 14:16:28 +0700

    register dagger application

--- app/src/main/java/com/github/syafiqq/androidmvptest001/controller/App.kt
+++ app/src/main/java/com/github/syafiqq/androidmvptest001/controller/App.kt
@@ -1,11 +1,23 @@
 package com.github.syafiqq.androidmvptest001.controller
 
-import android.app.Application
 import android.util.Log
 import com.github.syafiqq.androidmvptest001.BuildConfig
+import com.github.syafiqq.androidmvptest001.model.di.component.AppComponent
+import com.github.syafiqq.androidmvptest001.model.di.component.DaggerAppComponent
+import com.github.syafiqq.ext.dagger.android.DaggerApplication
+import dagger.android.AndroidInjector
 import timber.log.Timber
 
-class App : Application() {
+class App : DaggerApplication() {
+    override fun applicationInjector(
+        cls: Class<*>?,
+        holders: MutableMap<Class<*>, Any>?
+    ): AndroidInjector<*> {
+        return DaggerAppComponent.factory().create(this)
+    }
+
+    override fun rootComponent(): Class<*> = AppComponent::class.java
+
     override fun onCreate() {
         super.onCreate()
         if (BuildConfig.DEBUG) {
