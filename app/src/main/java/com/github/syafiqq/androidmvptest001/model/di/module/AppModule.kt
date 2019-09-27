1f23d4c98db5e3564aa7031e2edcf83db8c18b05
Syafiq <syafiq.rezpector@gmail.com>
2019-09-27 08:23:19 +0700

    register services

--- app/src/main/java/com/github/syafiqq/androidmvptest001/model/di/module/AppModule.kt
+++ app/src/main/java/com/github/syafiqq/androidmvptest001/model/di/module/AppModule.kt
@@ -4,6 +4,10 @@ import android.content.Context
 import com.github.syafiqq.androidmvptest001.controller.App
 import com.github.syafiqq.androidmvptest001.model.concurrent.SchedulerProvider
 import com.github.syafiqq.androidmvptest001.model.concurrent.SchedulerProviderImpl
+import com.github.syafiqq.androidmvptest001.model.service.api.identity.IdentityServer
+import com.github.syafiqq.androidmvptest001.model.service.api.identity.IdentityServerImpl
+import com.github.syafiqq.androidmvptest001.model.service.identity.UserManager
+import com.github.syafiqq.androidmvptest001.model.service.identity.UserManagerImpl
 import dagger.Binds
 import dagger.Module
 import javax.inject.Singleton
@@ -17,6 +21,14 @@ abstract class AppModule {
     @Binds
     @Singleton
     abstract fun provideApplicationContext(app: App): Context
+
+    @Binds
+    @Singleton
+    abstract fun provideUserManager(app: UserManagerImpl): UserManager
+
+    @Binds
+    @Singleton
+    abstract fun provideIdentityServer(app: IdentityServerImpl): IdentityServer
 }
 
 @Module
