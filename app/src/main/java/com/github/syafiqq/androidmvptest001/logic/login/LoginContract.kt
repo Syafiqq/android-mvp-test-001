50f43909c09b3838173fcd954b93beadd4f2123c
Syafiq <syafiq.rezpector@gmail.com>
2019-09-27 08:29:56 +0700

    add LoginContract

---
+++ app/src/main/java/com/github/syafiqq/androidmvptest001/logic/login/LoginContract.kt
@@ -0,0 +1,23 @@
+package com.github.syafiqq.androidmvptest001.logic.login
+
+import com.github.syafiqq.androidmvptest001.model.entity.UserEntity
+import com.github.syafiqq.androidmvptest001.model.mvp.contract.ActivityLifecycleContract
+
+interface LoginContract {
+    interface View : ActivityLifecycleContract.View {
+        fun onLoginClick()
+        fun onInvalidEmail(cause: String)
+        fun onInvalidPassword(cause: String)
+        fun onSuccessLogin(user: UserEntity)
+        fun onFailedLogin(cause: String)
+        fun onErrorLogin(ex: Throwable)
+        fun onCompleteLogin()
+        fun onCancelLogin()
+        fun gotoHomeActivity()
+    }
+
+    interface Presenter : ActivityLifecycleContract.Presenter {
+        fun doLogin(email: String? = null, password: String? = null)
+        fun doCancelLogin()
+    }
+}
