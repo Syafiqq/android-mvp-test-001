6b2dae25ad5f8bee743c934183c18a10affb75cf
Syafiq <syafiq.rezpector@gmail.com>
2019-09-28 12:07:59 +0700

    add home activity

---
+++ app/src/main/java/com/github/syafiqq/androidmvptest001/logic/home/HomeActivity.kt
@@ -0,0 +1,142 @@
+package com.github.syafiqq.androidmvptest001.logic.home
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.github.syafiqq.androidmvptest001.R
+import com.github.syafiqq.androidmvptest001.model.di.component.UserComponent
+import com.github.syafiqq.ext.dagger.android.AndroidInjection
+import io.reactivex.Observer
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
+import io.reactivex.subjects.PublishSubject
+import kotlinx.android.synthetic.main.activity_home.*
+import timber.log.Timber
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
+
+class HomeActivity : AppCompatActivity(), HomeContract.View {
+    @Inject
+    lateinit var presenter: HomeContract.Presenter
+    private var disposable = CompositeDisposable()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Timber.d("onCreate [savedInstanceState: Bundle?]")
+
+        AndroidInjection.inject(this, UserComponent::class.java)
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_home)
+        setSupportActionBar(toolbar)
+
+
+        val s: Observer<View> = PublishSubject.create<View>().apply {
+            throttleFirst(350, TimeUnit.MILLISECONDS)
+            subscribe { presenter.onMailClicked() }?.addTo(disposable)
+        }
+
+        fab.setOnClickListener(s::onNext)
+        presenter.onCreate()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        Timber.d("onCreate")
+        super.onCreate(savedInstanceState, persistentState)
+
+        presenter.onCreateWithPersistence()
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        Timber.d("onPostCreate [savedInstanceState: Bundle?]")
+        super.onPostCreate(savedInstanceState)
+
+        presenter.onPostCreate()
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        Timber.d("onPostCreate [savedInstanceState: Bundle?, persistentState: PersistableBundle?]")
+        super.onPostCreate(savedInstanceState, persistentState)
+
+        presenter.onPostCreateWithPersistence()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        Timber.d("onSaveInstanceState [outState: Bundle]")
+        super.onSaveInstanceState(outState)
+
+        presenter.onSaveInstanceState()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        Timber.d("onSaveInstanceState [outState: Bundle, outPersistentState: PersistableBundle]")
+        super.onSaveInstanceState(outState, outPersistentState)
+
+        presenter.onSaveInstanceStateWithPersistence()
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        Timber.d("onRestoreInstanceState [savedInstanceState: Bundle]")
+        super.onRestoreInstanceState(savedInstanceState)
+
+        presenter.onRestoreInstanceState()
+    }
+
+    override fun onRestoreInstanceState(
+        savedInstanceState: Bundle?,
+        persistentState: PersistableBundle?
+    ) {
+        Timber.d("onRestoreInstanceState [savedInstanceState: Bundle?, persistentState: PersistableBundle?]")
+        super.onRestoreInstanceState(savedInstanceState, persistentState)
+
+        presenter.onRestoreInstanceStateWithPersistence()
+    }
+
+    override fun onStart() {
+        Timber.d("onStart")
+        super.onStart()
+
+        presenter.onStart()
+    }
+
+    override fun onResume() {
+        Timber.d("onResume")
+        super.onResume()
+
+        presenter.onResume()
+    }
+
+    override fun onRestart() {
+        Timber.d("onRestart")
+        super.onRestart()
+
+        presenter.onRestart()
+    }
+
+    override fun onPause() {
+        Timber.d("onPause")
+        super.onPause()
+
+        presenter.onPause()
+    }
+
+    override fun onStop() {
+        Timber.d("onStop")
+        presenter.onStop()
+
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Timber.d("onDestroy")
+        presenter.onDestroy()
+        disposable.dispose()
+
+        super.onDestroy()
+    }
+
+    override fun gotoDetailActivity() {
+        Timber.d("gotoDetailActivity")
+    }
+
+}
